c790bd39a969ef5032fc2395c5d0e3f2c014c3eb
Ximenitap <123569520+Ximenitap@users.noreply.github.com>
2023-05-29 11:56:29 -0500

    29 de mayo fnerng

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const cuadrado = document.querySelectorAll(".cuadrado";
 const tiempofaltante = document.querySelector("#tiempo")
 let puntaje = document.getElementById("puntaje");
+
+let resultado= 0;
+let tiempoactual = tiempofaltante.textContent;
+let posiciontopo = null;
+
+function cuadradoazar() {
+  cuadrado.forEach((nombre del clase) => {
+    nombredelaclase.classlist.remover("topo");
+  
+  });
+
+let posicionalazar = cuadrado 
