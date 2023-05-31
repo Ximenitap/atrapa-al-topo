da8ba0fc8acd37304fd92f7c627e88509f39777e
Ximenitap <123569520+Ximenitap@users.noreply.github.com>
2023-05-31 10:28:01 -0500

    30/05/23

--- app.js
+++ app.js
@@ -10,6 +10,36 @@ function cuadradoazar() {
   cuadrado.forEach((nombre del clase) => {
     nombredelaclase.classlist.remover("topo");
   
-  });
+  })
 
-let posicionalazar = cuadrado 
+  }
+  let posicionalazar = cuadrado[Math.floor (Math.random()*9];
+  posicionalazar.classList.add("topo");}
+  
+     posiciontopo = posicionalazar.id;
+  }
+  
+  cuadrado.forEach((identificador) => {
+    identificador.addEventListener("click", () => {
+      if (identificador.id === posiciontopo) {
+        resultado = resultado + 1;
+        puntaje.textContent = resultado;
+        posicionalazar = null;
+      }
+    });
+  }); 
+  function movertopo(){
+    tiempotopo = setInterval(cuadroazar, 700);
+  }
+  movertopo()
+
+  function cuentaregresiva(){
+    tiempoactual--;
+    tiempofaltante.textContent = tiempo actual;
+    if (tiempoactual=== 0){
+      clearInterval(idtiempo);
+      clearInterval(tiempotopo);
+      alert("se acabó el tiempo, tu puntaje es" + resultado + "topos atrapados");
+    }
+    }
+  
