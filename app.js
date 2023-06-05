eab3d5b8bc5794273ecebff69cee869044ed12b3
Ximenitap <123569520+Ximenitap@users.noreply.github.com>
2023-06-05 11:56:49 -0500

    SI

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-const cuadrado = document.querySelectorAll(".cuadrado";
-const tiempofaltante = document.querySelector("#tiempo")
+const cuadrado = document.querySelectorAll(".cuadrado");
+const tiempofaltante = document.querySelector("#tiempo");
 let puntaje = document.getElementById("puntaje");
 
-let resultado= 0;
+let resultado = 0;
 let tiempoactual = tiempofaltante.textContent;
-let posiciontopo = null;
 
 function cuadradoazar() {
-  cuadrado.forEach((nombre del clase) => {
-    nombredelaclase.classlist.remover("topo");
-  
-  })
+  cuadrado.forEach((nombredeclase) => {
+    nombredeclase.classList.remove("topo");
+  });
+  let posicionalazar = cuadrado[Math.floor(Math.random() * 9)];
+  posicionalazar.classList.add("topo");
 
-  }
-  let posicionalazar = cuadrado[Math.floor (Math.random()*9];
-  posicionalazar.classList.add("topo");}
-  
-     posiciontopo = posicionalazar.id;
-  }
-  
-  cuadrado.forEach((identificador) => {
-    identificador.addEventListener("click", () => {
-      if (identificador.id === posiciontopo) {
-        resultado = resultado + 1;
-        puntaje.textContent = resultado;
-        posicionalazar = null;
-      }
-    });
-  }); 
-  function movertopo(){
-    tiempotopo = setInterval(cuadroazar, 700);
-  }
-  movertopo()
+  posiciontopo = posicionalazar.id;
+}
 
-  function cuentaregresiva(){
-    tiempoactual--;
-    tiempofaltante.textContent = tiempo actual;
-    if (tiempoactual=== 0){
-      clearInterval(idtiempo);
-      clearInterval(tiempotopo);
-      alert("se acabó el tiempo, tu puntaje es" + resultado + "topos atrapados");
+cuadrado.forEach((identificador) => {
+  identificador.addEventListener("click", () => {
+    if (identificador.id === posiciontopo) {
+      resultado = resultado + 1;
+      puntaje.textContent = resultado;
+      posicionalazar = null;
     }
-    }
-  
+  });
+});
+
+function movertopo() {
+  let tiempotopo = setInterval(cuadradoazar, 700);
+}
+movertopo();
+
+function cuentaregresiva() {
+  tiempoactual--;
+  tiempofaltante.textContent = tiempoactual;
+  if (tiempoactual === 0) {
+    clearInterval(idtiempo);
+    clearInterval(tiempotopo);
+    alert(
+      "Se acabó el tiempo, tu puntaje es " + resultado + " topos atrapados"
+    );
+  }
+}
+let idtiempo = setInterval(cuentaregresiva, 1000);
