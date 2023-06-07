472b2c169116d0cc9dc5bb6229432fa10d923d37
Ximenitap <123569520+Ximenitap@users.noreply.github.com>
2023-06-07 10:00:38 -0500

    7/06/23 trabajo final

--- app.js
+++ app.js
@@ -1,44 +1,88 @@
 const cuadrado = document.querySelectorAll(".cuadrado");
+
 const tiempofaltante = document.querySelector("#tiempo");
+
 let puntaje = document.getElementById("puntaje");
 
+ 
+
 let resultado = 0;
+
 let tiempoactual = tiempofaltante.textContent;
 
+let posiciontopo = null;
+
+ 
+
 function cuadradoazar() {
+
   cuadrado.forEach((nombredeclase) => {
+
     nombredeclase.classList.remove("topo");
+
   });
+
+ 
+
   let posicionalazar = cuadrado[Math.floor(Math.random() * 9)];
+
   posicionalazar.classList.add("topo");
 
+ 
+
   posiciontopo = posicionalazar.id;
+
 }
+
+ 
 
 cuadrado.forEach((identificador) => {
+
   identificador.addEventListener("click", () => {
+
     if (identificador.id === posiciontopo) {
+
       resultado = resultado + 1;
+
       puntaje.textContent = resultado;
+
       posicionalazar = null;
+
     }
+
   });
+
 });
 
+ 
+
 function movertopo() {
-  let tiempotopo = setInterval(cuadradoazar, 700);
+
+  tiempotopo = setInterval(cuadradoazar, 700);
+
 }
+
 movertopo();
 
+ 
+
 function cuentaregresiva() {
+
   tiempoactual--;
+
   tiempofaltante.textContent = tiempoactual;
+
   if (tiempoactual === 0) {
+
     clearInterval(idtiempo);
+
     clearInterval(tiempotopo);
-    alert(
-      "Se acabó el tiempo, tu puntaje es " + resultado + " topos atrapados"
-    );
+
+    alert("Se acabó el tiempo, tu puntaje es " + resultado + " topos atrapados");
+
   }
+
 }
+
 let idtiempo = setInterval(cuentaregresiva, 1000);
+
